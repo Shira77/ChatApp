aef79792e6521e94cc40fc00aae6c5480a6036ba
tamar6465 <tamarf6465@gmail.com>
2023-08-23 15:54:56 +0300

    Shiri wednesday

--- chatApp.py
+++ chatApp.py
@@ -1,7 +1,33 @@
-from flask import Flask
+import csv
+from flask import Flask, render_template, request, redirect
 server = Flask(__name__)
-@server.route("/")
+
+
+def chechUserExist(username,password):
+  with open('users.csv', "r") as usersExist:
+    users=csv.reader(usersExist)
+  for user in users:
+      if(user[0] == username and user[1] == password):
+          return True 
+  return False  
+
+@server.route("/login", methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if(chechUserExist(username, password)):
+          return redirect('lobby')
+
+       
     return render_template('login.html')
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
+    server.run(host='0.0.0.0', port="5000", debug=True)
+    
+    
+    
+@server.route("/lobby", methods=['GET','POST'])
+def lobbyPage():
+        return render_template('login.html')    
+    
+    
