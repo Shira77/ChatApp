4764d7e1613404cd75a4bae1b0a74f15a379915b
tamar6465 <tamarf6465@gmail.com>
2023-08-24 11:30:58 +0300

    חמישי בוקר

--- chatApp.py
+++ chatApp.py
@@ -1,14 +1,16 @@
+from flask import Flask, render_template, request, redirect,session
 import csv
-from flask import Flask, render_template, request, redirect
-server = Flask(__name__)
+import base64
+import os
 
+server = Flask(__name__) 
 
 def chechUserExist(username,password):
-  with open('users.csv', "r") as usersExist:
+  with open('users.csv', 'r', newline='') as usersExist:
     users=csv.reader(usersExist)
-  for user in users:
-      if(user[0] == username and user[1] == password):
-          return True 
+    for user in users:
+        if(user[0] == username and user[1] == password):
+           return True 
   return False  
 
 @server.route("/login", methods=['GET','POST'])
@@ -16,18 +18,17 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if(chechUserExist(username, password)):
+        b=chechUserExist(username, password)
+        if(b):
+          session['username'] = username
           return redirect('lobby')
-
-       
     return render_template('login.html')
+#אתחול החדרים הקיימים 
+rooms = []
+for filename in os.listdir("rooms"):
+    if filename.endswith(".txt"):
+       room_name = filename[:-4]
+       rooms.append(room_name)   
+    
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port="5000", debug=True)
-    
-    
-    
-@server.route("/lobby", methods=['GET','POST'])
-def lobbyPage():
-        return render_template('login.html')    
-    
-    
+    server.run(host='0.0.0.0', port="5000")
