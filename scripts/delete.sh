e81a4471e9ff529fe707a9f1929cc050a7fc0f4d
tamar6465 <tamarf6465@gmail.com>
2023-09-26 10:36:41 +0300

    modify script files

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Stop all running containers
-docker stop $(docker ps -a -q)
 
-# Remove all stopped containers
-docker rm $(docker ps -a -q)
 
-#Remove volume my volume
-docker volume remove my-volume
-
-# Remove all images
-docker rmi -f $(docker images -a -q)
-
-# remove all images which are not used by existing containers
-docker image prune -a
+docker stop chat-app-run
+if [ $# -eq 0 ]; then
+    docker rmi -f chat-app
+else
+    docker rmi -f chat-app:$1
+fi
+docker rm -f chat-app-run
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-docker build -t myimage .
-docker run -d -p 5000:5000 myimage
+version='latest'
+if [ $# -ne 0 ]; then
+  # Arguments were passed, so use them
+    version=$1
+fi
+
+
+docker volume create chat-app-data
+docker build -t  chat-app:${version} .
+docker run -v chat-app-data:/chatApp/data -p 5000:5000 --name chat-App-run chat-app:${version}
 
 
 
